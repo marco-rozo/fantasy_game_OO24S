6e1b584f2f97ae0d115460bf94501a750337e378
marco-rozo <marcorozo99@gmail..com>
2022-05-06 19:47:33 -0300

    Criando projeto com spring

---
+++ src/main/java/br/edu/utfpr/fanstasygame/FantasyGameApplication.java
@@ -0,0 +1,11 @@
+package br.edu.utfpr.fanstasygame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyGameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyGameApplication.class, args);
+    }
+}
