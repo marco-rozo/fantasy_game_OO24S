6a2a0c85abe2a59eedb90d68b75b972dd1e3e4a2
marco-rozo <marcorozo99@gmail..com>
2022-05-13 19:33:07 -0300

    Criando model de usuarios

---
+++ src/main/java/br/edu/utfpr/fanstasygame/model/Users.java
@@ -0,0 +1,30 @@
+package br.edu.utfpr.fanstasygame.model;
+
+import lombok.*;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.UUID;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Users {
+
+    @Id @GeneratedValue(strategy = AUTO) @Type(type = "uuid-char")
+    private UUID id;
+
+    private String name;
+    private String lastName;
+    private String email;
+    private String password;
+}
