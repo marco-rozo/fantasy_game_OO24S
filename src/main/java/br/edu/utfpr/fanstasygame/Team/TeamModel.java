04c2462ed64a8161b3c643dd0fd83afd8e4874aa
marco-rozo <marcorozo99@gmail..com>
2022-06-06 20:39:12 -0300

    Adicionando relação many-to-many entre time e jogadores

--- src/main/java/br/edu/utfpr/fanstasygame/Team/TeamModel.java
+++ src/main/java/br/edu/utfpr/fanstasygame/Team/TeamModel.java
@@ -1,11 +1,14 @@
 package br.edu.utfpr.fanstasygame.Team;
 
+import br.edu.utfpr.fanstasygame.Player.PlayerModel;
 import br.edu.utfpr.fanstasygame.User.UserModel;
 import br.edu.utfpr.fanstasygame.Leagues.LeagueModel;
 import lombok.*;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -24,10 +27,14 @@ public class TeamModel {
 
     @OneToOne
     private UserModel user;
+
     private LocalDateTime createdAt;
 
     @ManyToOne //atribuindo muitos times para uma liga
     @JoinColumn(name = "league_id")
     private LeagueModel league;
+
+    @ManyToMany
+    private List<PlayerModel> players;
 
 }
