ef55a5eb147e0a36abbd72cf215d39ec3e385e47
marco-rozo <marcorozo99@gmail..com>
2022-06-06 20:46:52 -0300

    Adicionando testes na main

--- src/main/java/br/edu/utfpr/fanstasygame/FantasyGameApplication.java
+++ src/main/java/br/edu/utfpr/fanstasygame/FantasyGameApplication.java
@@ -1,11 +1,44 @@
 package br.edu.utfpr.fanstasygame;
 
+import br.edu.utfpr.fanstasygame.Team.TeamModel;
+import br.edu.utfpr.fanstasygame.Team.TeamRepository;
+import br.edu.utfpr.fanstasygame.Team.TeamService;
+import br.edu.utfpr.fanstasygame.User.UserModel;
+import br.edu.utfpr.fanstasygame.User.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class FantasyGameApplication {
+    @Autowired
+    private TeamService teamService;
+    @Autowired
+    private UserRepository userRepository;
     public static void main(String[] args) {
         SpringApplication.run(FantasyGameApplication.class, args);
     }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void doSomethingAfterStartup() {
+        var user = userRepository.saveAndFlush(UserModel.builder()
+                        .name("Marco")
+                        .lastName("Rozo")
+                        .createdAt(LocalDateTime.now())
+                        .email("marcorozo@gmail.com")
+                        .password("123123123")
+                        .build()
+        );
+
+        teamService.createTeam(TeamModel.builder()
+                .name("HRomeuPowerGuido")
+                .createdAt(LocalDateTime.now())
+                .user(user)
+                .build());
+    }
+
 }
